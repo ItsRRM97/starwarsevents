4a72b7444479bafecea886c2ecb31678af63f7e8
Roshan Raj Mishra <mishra.roshanraj@gmail.com>
2017-05-07 21:27:14 +0530

    Twig

--- src/Yoda/EventBundle/Controller/DefaultController.php
+++ src/Yoda/EventBundle/Controller/DefaultController.php
@@ -3,22 +3,14 @@
 namespace Yoda\EventBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     public function indexAction($firstName, $count)
     {
-    	$data = array(
-    		'count' => $count,
-    		'firstName' => $firstName,
-    		'ackbar' => 'Its a traaaaap!'
-    		);
-    	$json=json_encode($data);
-        
-        $response = new Response($json);
-        $response->headers->set('Content-Type','application/json');
-
-        return $response;
+  
+    		return $this->render('EventBundle:Default:index.html.twig',
+    			array('name' => $firstName)
+    			);
+    	}
     }
-}
