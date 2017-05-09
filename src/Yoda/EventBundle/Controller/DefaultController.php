aa9acad82a06b67c88f1472b6914b98e4ae54a32
Roshan Raj Mishra <mishra.roshanraj@gmail.com>
2017-05-10 02:04:13 +0530

    Error Encountered, needs fix

--- src/Yoda/EventBundle/Controller/DefaultController.php
+++ src/Yoda/EventBundle/Controller/DefaultController.php
@@ -8,6 +8,10 @@ class DefaultController extends Controller
 {
     public function indexAction($firstName, $count)
     {
+    	$em = $this->container->get('doctrine')->getManager();
+    	$repo = $em->getDoctrine()->getManager();
+
+    	$event = $repo->findOneBy(array('name'=>'Darth\'s surprise birthday party!'));
   
     		return $this->render('EventBundle:Default:index.html.twig',
     			array('name' => $firstName)
