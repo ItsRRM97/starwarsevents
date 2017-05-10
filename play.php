8aed7001c44887a9669d681f9911d6f325cd7c58
Roshan Raj Mishra <mishra.roshanraj@gmail.com>
2017-05-10 14:52:07 +0530

    Work On Windows

--- play.php
+++ play.php
@@ -4,7 +4,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Debug\Debug;
 
 /** @var \Composer\Autoload\ClassLoader $loader */
-$loader = require __DIR__.'/../app/autoload.php';
+$loader = require_once __DIR__.'/../app/autoload.php';
 Debug::enable();
 
 $kernel = new AppKernel('dev', true);
@@ -18,5 +18,13 @@ $container->set('request',$request);
 
 //all the setup is done!!
 
-$templating=$container->gt('templating');
-echo $templating->render('EventBundle:Default:index.html.twig',array('name' => 'Vader', 'count'=> 3));
+use Yoda\EventBundle\Entity\Events;
+
+$event = new Events();
+$event -> setName('Darth\'s surprise Birthday Party!');
+$event -> setLocation('Deathstar');
+$event -> setTime(new \DateTime('Tomorrow Noon!'));
+//$event->setDetails('Ha! Darth Hates Supirses!!!!');
+
+//$templating=$container->gt('templating');
+//echo $templating->render('EventBundle:Default:index.html.twig',array('name' => 'Vader', 'count'=> 3));
--- src/Yoda/EventBundle/Controller/DefaultController.php
+++ src/Yoda/EventBundle/Controller/DefaultController.php
@@ -8,13 +8,14 @@ class DefaultController extends Controller
 {
     public function indexAction($firstName, $count)
     {
-    	$em = $this->container->get('doctrine')->getManager();
-    	$repo = $em->getDoctrine()->getManager();
-
-    	$event = $repo->findOneBy(array('name'=>'Darth\'s surprise birthday party!'));
+    	//$em = $this->container->get('doctrine')->getManager();
+    	$em = $this->getDoctrine()->getManager();
+        $repo = $em->getRepository('EventBundle:Events');
+        
+            	$event = $repo->findOneBy(array('name'=>'Darth\'s surprise birthday party!'));
   
     		return $this->render('EventBundle:Default:index.html.twig',
-    			array('name' => $firstName)
+    			array('name' => $firstName, 'count' => $count, 'event' => $event)
     			);
     	}
     }
